a199a5c60b10898bff29f39cd301abf0db8b31a5
Almir Stark <Almirwebdev@gmail.com>
2022-03-16 13:08:07 -0300

    Update App.js

--- App.js
+++ App.js
@@ -6,17 +6,26 @@ import {
     ScrollView,
     StyleSheet,
     View,
-    Text
+    Button,
+    Text,
+    StatusBar,
+    Linking,
+    TouchableOpacity,
 } from 'react-native';
+
+ 
 
 const { width } = Dimensions.get('window');
 const headerHeight = 300;
 const headerFinalHeight = 60;
 const imageSize = (headerHeight / 4) * 2;
 
+
+
 export default function ScrollViewAnimatedHeader() {
     const scrollY = useRef(new Animated.Value(0)).current;
     const [textWidth, setTextWidth] = useState(0);
+  
     const offset = headerHeight - headerFinalHeight;
     const translateHeader = scrollY.interpolate({
         inputRange: [0, offset],
@@ -53,6 +62,8 @@ export default function ScrollViewAnimatedHeader() {
     });
     return (
         <View style={styles.container}>
+            <StatusBar
+                backgroundColor="#3a0164" />
             <ScrollView
                 contentContainerStyle={styles.scrollContainer}
                 showsVerticalScrollIndicator={false}
@@ -62,8 +73,17 @@ export default function ScrollViewAnimatedHeader() {
                 )}>
                 {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(x => (
                     <View style={styles.item} key={x} />
-                ))}
+                ))} 
+                <View style={styles.BoxInfor}>
+                         <Text style={styles.BoxInforNome}>Feito por Almir Stark  </Text>
+                   
+                </View>
             </ScrollView>
+            <TouchableOpacity style={styles.BoxInforGit}>
+            <Text style={styles.BtnText}>Git </Text>
+            
+
+                         </TouchableOpacity>
             <Animated.View
                 pointerEvents='none'
                 style={[styles.header, { transform: [{ translateY: translateHeader }] }]}>
@@ -117,14 +137,15 @@ const styles = StyleSheet.create({
     item: {
         height: 100,
         marginBottom: 5,
+        borderRadius: 5,
         backgroundColor: 'grey',
-        marginHorizontal: 10,
+        marginHorizontal: 5,
     },
     header: {
         height: headerHeight,
         backgroundColor: '#4B0082',
-position: 'absolute',
-        width: '100%',
+        position: 'absolute',
+         width: '100%',
         alignItems: 'center',
         justifyContent: 'center',
     },
@@ -135,7 +156,7 @@ position: 'absolute',
         height: imageSize,
         width: imageSize,
         borderRadius: headerHeight,
-        backgroundColor: 'red',
+         backgroundColor: 'red',
     },
     img: {
        height: imageSize,
@@ -164,5 +185,42 @@ position: 'absolute',
     FuncaoHeader: {
       color: "#fff",
       fontSize: 15,
+    },
+    BoxInfor:{
+        backgroundColor: "#000",
+        height: 30,
+        alignItems: 'center',
+        display: 'flex',
+        width: "100%",
+        textAlign: 'center',
+    },
+    BoxInforNome:{
+        color: "#fff",
+        top: 5,
+    },
+    BoxInforGit:{
+        backgroundColor:"#4B0082",
+        display: "flex",
+        alignItems: "center",
+        paddingVertical: 10,
+        position: "absolute",
+        width: 50,
+        height: 50,
+        bottom: 5,
+        right: 5,
+        borderRadius: 50,
+        elevation: 5,
+        shadowOffset:{
+            width:2,
+            height:2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.5,
+    },
+    BtnText:{
+        color: "#fff",
+        fontSize: 22,
     }
 });
+
+//BY ALMIR STARK
